598be03f8cc127b676082c6f487680662762dd5e
Mateo Torres <mateot659@gmail.com>
2025-11-25 11:40:57 -0500

    add

--- compute.c
+++ compute.c
@@ -3,33 +3,30 @@
 #include "vector.h"
 #include "config.h"
 
+
 //compute: Updates the positions and locations of the objects in the system based on gravity.
 //Parameters: None
 //Returns: None
 //Side Effect: Modifies the hPos and hVel arrays with the new positions and accelerations after 1 INTERVAL
 void compute(){
 	//make an acceleration matrix which is NUMENTITIES squared in size;
+	
 	int i,j,k;
+	
 	vector3* values=(vector3*)malloc(sizeof(vector3)*NUMENTITIES*NUMENTITIES);
 	vector3** accels=(vector3**)malloc(sizeof(vector3*)*NUMENTITIES);
+	
+	//init accels
 	for (i=0;i<NUMENTITIES;i++)
 		accels[i]=&values[i*NUMENTITIES];
+
 	//first compute the pairwise accelerations.  Effect is on the first argument.
-	for (i=0;i<NUMENTITIES;i++){
-		for (j=0;j<NUMENTITIES;j++){
-			if (i==j) {
-				FILL_VECTOR(accels[i][j],0,0,0);
-			}
-			else{
-				vector3 distance;
-				for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
-				double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
-				double magnitude=sqrt(magnitude_sq);
-				double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
-				FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
-			}
-		}
-	}
+	
+	dim3 threadsPerBlock(16, 16);
+	dim3 nBlocks(NUMENTITIES / threadsPerBlock.x, NUMENTITIES / threadsPerBlock.y);
+
+	pairwiseAcceleration << nBlocks, threadsPerBlock >> (accels);
+
 	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
 	for (i=0;i<NUMENTITIES;i++){
 		vector3 accel_sum={0,0,0};
@@ -46,4 +43,20 @@ void compute(){
 	}
 	free(accels);
 	free(values);
+}
+
+
+__global__ void pairwiseAcceleration(vector3** accels) {
+	
+	if (i == j) {
+		FILL_VECTOR(accels[i][j], 0, 0, 0);
+	}
+	else {
+		vector3 distance;
+		for (k = 0; k < 3; k++) distance[k] = hPos[i][k] - hPos[j][k];
+		double magnitude_sq = distance[0] * distance[0] + distance[1] * distance[1] + distance[2] * distance[2];
+		double magnitude = sqrt(magnitude_sq);
+		double accelmag = -1 * GRAV_CONSTANT * mass[j] / magnitude_sq;
+		FILL_VECTOR(accels[i][j], accelmag * distance[0] / magnitude, accelmag * distance[1] / magnitude, accelmag * distance[2] / magnitude);
+	}
 }

30a2879ba5843c908132108ecf4eebf419f8bdad
Mateo Torres <mateot659@gmail.com>
2025-11-30 10:37:05 -0500

    a

--- reduction.c
+++ reduction.c
@@ -2,27 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sum_one_to_n(int n) {
-	return n * (n + 1) / 2;
-}
-
 int main() {
-	int N = 1 << 20;
+	int N = 1 << 30;
 
 	int* arr = (int*)malloc(N * sizeof(int));
 
 	for (int i = 0; i < N; i++) {
-		arr[i] = i;
+		arr[i] = 1;
 	}
 
-
-
+	
 	int sum = 0;
 	for (int i = 0; i < N; i++) {
 		sum += arr[i];
 	}
 
-	printf("N = %d\n SUM = %d\n EXPECTED_SUM = %d\n", N, sum, sum_one_to_n(N));
+	printf("N = %d\n SUM = %d", N, sum);
 
+
+	free(arr);
 	return 0;
 }

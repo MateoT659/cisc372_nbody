78ccfac6838823e7046e8d51659f835ad26737d8
Mateo Torres <mateot659@gmail.com>
2025-11-30 10:14:18 -0500

    aa

--- reduction.c
+++ reduction.c
@@ -4,7 +4,18 @@
 int main() {
 	int N = 1 << 20;
 
-	printf("N = %d\n", N);
+	int* arr = (int*) malloc(N*sizeof(int))
 
+	for (int i = 0; i < N; i++) {
+		arr[i] = 1;
+	}
 
+	int sum = 0;
+	for (int i = 0; i < N; i++) {
+		sum += arr[i];
+	}
+
+	printf("N = %d\n, SUM = %d\n", N, sum);
+
+	return 0;
 }

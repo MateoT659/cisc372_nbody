77bf3a4cdbb161e8e288f4dc66924d561da60476
Mateo Torres <mtorres@udel.edu>
2025-12-01 17:39:55 -0500

    asdas

--- compute.cu
+++ compute.cu
@@ -10,6 +10,24 @@ void initAccels(vector3** accels, vector3* values) {
 	accels[i] = &values[i * NUMENTITIES];
 }
 
+__global__
+void pairwiseAccels(vector3** accels, vector3* hPos, double* mass) {
+	int i = threadIdx.x + blockIdx.x * blockDim.x;
+	int j = threadIdx.y + blockIdx.y * blockDim.y;
+
+	if (i == j) {
+		FILL_VECTOR(accels[i][j], 0, 0, 0);
+	}
+	else {
+		vector3 distance;
+		for (int k = 0; k < 3; k++) distance[k] = hPos[i][k] - hPos[j][k];
+		double magnitude_sq = distance[0] * distance[0] + distance[1] * distance[1] + distance[2] * distance[2];
+		double magnitude = sqrt(magnitude_sq);
+		double accelmag = -1 * GRAV_CONSTANT * mass[j] / magnitude_sq;
+		FILL_VECTOR(accels[i][j], accelmag * distance[0] / magnitude, accelmag * distance[1] / magnitude, accelmag * distance[2] / magnitude);
+	}
+}
+
 extern "C" void compute() {
 	//make an acceleration matrix which is NUMENTITIES squared in size;
 	int i, j, k;
@@ -28,30 +46,20 @@ extern "C" void compute() {
 	cudaMalloc(&d_values, sizeof(vector3) * NUMENTITIES * NUMENTITIES);
 	cudaMalloc(&d_accels, sizeof(vector3) * NUMENTITIES);
 
-	int block_size = NUMENTITIES;
+	int threads_per_block_init = NUMENTITIES;
 
+	initAccels << <1, threads_per_block_init >> > (d_accels, d_values);
 	
 	
-	
-	
-	
-	//first compute the pairwise accelerations.  Effect is on the first argument.
-	for (i = 0; i < NUMENTITIES; i++) {
-		for (j = 0; j < NUMENTITIES; j++) {
-			if (i == j) {
-				FILL_VECTOR(accels[i][j], 0, 0, 0);
-			}
-			else {
-				vector3 distance;
-				for (k = 0; k < 3; k++) distance[k] = hPos[i][k] - hPos[j][k];
-				double magnitude_sq = distance[0] * distance[0] + distance[1] * distance[1] + distance[2] * distance[2];
-				double magnitude = sqrt(magnitude_sq);
-				double accelmag = -1 * GRAV_CONSTANT * mass[j] / magnitude_sq;
-				FILL_VECTOR(accels[i][j], accelmag * distance[0] / magnitude, accelmag * distance[1] / magnitude, accelmag * distance[2] / magnitude);
-			}
-		}
-	}
-	
+	cudaMemcpy(d_hPos, hPos, NUMENTITIES * sizeof(vector3), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_hVel, hVel, NUMENTITIES * sizeof(vector3), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_mass, mass, NUMENTITIES * sizeof(double), cudaMemcpyHostToDevice);
+
+
+	dim3 threads_per_block_PWA(16, 16);
+	dim3 n_blocks_PWA((NUMENTITIES + threads_per_block_PWA.x - 1) / threads_per_block_PWA.x, (NUMENTITIES + threads_per_block_PWA.y - 1) / threads_per_block_PWA.y);
+
+	pairwiseAccels<<<n_blocks_PWA, threads_per_block_PWA >>>(d_accels, d_hPos, d_mass);
 	
 	
 	
@@ -70,7 +78,11 @@ extern "C" void compute() {
 		}
 	}
 	
-	
+	cudaFree(d_values);
+	cudaFree(d_accels);
+	cudaFree(d_hPos);
+	cudaFree(d_hVel);
+	cudaFree(d_mass);
 	
 	free(accels);
 	free(values);

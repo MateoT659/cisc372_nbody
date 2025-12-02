f85f120d466605e155bcee42e12c6f0700649ce8
Mateo Torres <mtorres@udel.edu>
2025-12-02 16:53:42 -0500

    ASDASD

--- compute.cu
+++ compute.cu
@@ -18,7 +18,9 @@ inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort =
 }
 #define gpuCheckLastError() gpuErrchk(cudaGetLastError())
 
-//serial version
+// NOTE: initAccels kernel is no longer needed for correctness.
+// Keeping it commented out for reference.
+/*
 __global__
 void initAccels(vector3** accels, vector3* values, int n) {
 	int i = threadIdx.x + blockIdx.x * blockDim.x;
@@ -26,6 +28,7 @@ void initAccels(vector3** accels, vector3* values, int n) {
 		accels[i] = &values[i * n];
 	}
 }
+*/
 
 __global__
 void pairwiseAccels(vector3** accels, vector3* hPos, double* mass) {
@@ -39,9 +42,15 @@ void pairwiseAccels(vector3** accels, vector3* hPos, double* mass) {
 			vector3 distance;
 			for (int k = 0; k < 3; k++) distance[k] = hPos[i][k] - hPos[j][k];
 			double magnitude_sq = distance[0] * distance[0] + distance[1] * distance[1] + distance[2] * distance[2];
-			double magnitude = sqrt(magnitude_sq);
-			double accelmag = -1 * GRAV_CONSTANT * mass[j] / magnitude_sq;
-			FILL_VECTOR(accels[i][j], accelmag * distance[0] / magnitude, accelmag * distance[1] / magnitude, accelmag * distance[2] / magnitude);
+			// guard against zero-distance to avoid div-by-zero / NaNs
+			const double eps = 1e-12;
+			if (magnitude_sq <= eps) {
+				FILL_VECTOR(accels[i][j], 0, 0, 0);
+			} else {
+				double magnitude = sqrt(magnitude_sq);
+				double accelmag = -1.0 * GRAV_CONSTANT * mass[j] / magnitude_sq;
+				FILL_VECTOR(accels[i][j], accelmag * distance[0] / magnitude, accelmag * distance[1] / magnitude, accelmag * distance[2] / magnitude);
+			}
 		}
 	}
 }
@@ -67,13 +76,14 @@ void sumMatrices(vector3** accels, vector3* hVel, vector3* hPos) {
 
 extern "C" void compute() {
 	//make an acceleration matrix which is NUMENTITIES squared in size;
-
 	dim3 threads_per_block_PWA(16, 16);
 	dim3 n_blocks_PWA((NUMENTITIES + threads_per_block_PWA.x - 1) / threads_per_block_PWA.x, (NUMENTITIES + threads_per_block_PWA.y - 1) / threads_per_block_PWA.y);
 
-
 	pairwiseAccels<<<n_blocks_PWA, threads_per_block_PWA>>>(d_accels, d_hPos, d_mass);
-	gpuCheckLastError();
+	{
+		cudaError_t e = cudaGetLastError();
+		fprintf(stderr, "pairwiseAccels launch: %s (%s)\n", cudaGetErrorName(e), cudaGetErrorString(e));
+	}
 	gpuErrchk(cudaDeviceSynchronize());
 
 	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
@@ -81,7 +91,10 @@ extern "C" void compute() {
 	int n_blocks_update = (NUMENTITIES + threads_per_block_update - 1) / threads_per_block_update;
 
 	sumMatrices<<<n_blocks_update, threads_per_block_update>>>(d_accels, d_hVel, d_hPos);
-	gpuCheckLastError();
+	{
+		cudaError_t e = cudaGetLastError();
+		fprintf(stderr, "sumMatrices launch: %s (%s)\n", cudaGetErrorName(e), cudaGetErrorString(e));
+	}
 	gpuErrchk(cudaDeviceSynchronize());
 	
 }
@@ -94,15 +107,26 @@ extern "C" void initDeviceMemory(int numObjects)
 	gpuErrchk(cudaMalloc(&d_mass, sizeof(double) * numObjects));
 
 	// flat storage for N x N acceleration vectors
-	gpuErrchk(cudaMalloc(&d_values, sizeof(vector3) * numObjects * numObjects));
+	gpuErrchk(cudaMalloc(&d_values, sizeof(vector3) * (size_t)numObjects * (size_t)numObjects));
 	// allocate array of device pointers (vector3*)
 	gpuErrchk(cudaMalloc(&d_accels, sizeof(vector3*) * numObjects));
 
-	int threads_per_block_init = 256;
-	int n_blocks_init = (numObjects + threads_per_block_init - 1) / threads_per_block_init;
-	initAccels<<<n_blocks_init, threads_per_block_init>>>(d_accels, d_values, numObjects);
-	gpuCheckLastError();
-	gpuErrchk(cudaDeviceSynchronize());
+	// Build host-side pointer array that points into the contiguous d_values block,
+	// then copy that pointer-array to device. This is safer than writing device pointers
+	// from a kernel and avoids subtle bugs.
+	vector3 **h_accels = (vector3 **)malloc(sizeof(vector3*) * numObjects);
+	if (!h_accels) {
+		fprintf(stderr, "Failed to allocate host pointer array\n");
+		exit(1);
+	}
+	for (int i = 0; i < numObjects; ++i) {
+		// pointer arithmetic on device pointer is fine on the host as an offset value:
+		// host pointer value holds device pointer (address) returned by cudaMalloc.
+		h_accels[i] = d_values + (size_t)i * (size_t)numObjects;
+	}
+	// copy the array of device pointers into device memory
+	gpuErrchk(cudaMemcpy(d_accels, h_accels, sizeof(vector3*) * numObjects, cudaMemcpyHostToDevice));
+	free(h_accels);
 
 	//transfer generated values to device
 	gpuErrchk(cudaMemcpy(d_hPos, hPos, numObjects * sizeof(vector3), cudaMemcpyHostToDevice));

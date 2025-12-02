739179dc4fb02ec5f72015bcf970b7071fcb8809
Mateo Torres <mtorres@udel.edu>
2025-12-02 18:04:09 -0500

    ASDASD

--- nbody.c
+++ nbody.c
@@ -106,9 +106,7 @@ int main(int argc, char** argv)
 	for (t_now = 0; t_now < DURATION; t_now += INTERVAL) {
 		compute();
 		i++;
-		if (i % 5 == 0) {
-			printf("\n Completed %d intervals", i);
-		}
+		printf("\n Completed %d intervals", i);
 	}
 	clock_t t1 = clock() - t0;
 #ifdef DEBUG

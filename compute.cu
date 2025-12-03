d511e92cbf1deb03bbb18e7695e67f30669e291a
Mateo Torres <mtorres@udel.edu>
2025-12-02 19:46:43 -0500

    ASDSD

--- compute.cu
+++ compute.cu
@@ -4,6 +4,9 @@
 #include "vector.h"
 #include "config.h"
 
+vector3* d_values;
+vector3** d_accels;
+
 #define EC(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort = true)
 {
@@ -62,18 +65,6 @@ __global__ void accelSums(vector3** accels, vector3* hPos, vector3* hVel) {
 
 extern "C" void compute() {
 	//make an acceleration matrix which is NUMENTITIES squared in size;
-	vector3* d_values;
-	EC(cudaMalloc(&d_values, sizeof(vector3) * NUMENTITIES * NUMENTITIES));
-
-	vector3** d_accels;
-	EC(cudaMalloc(&d_accels, sizeof(vector3*) * NUMENTITIES));
-
-
-	int blockSize = 256;
-	int nBlocks = (NUMENTITIES + blockSize - 1) / blockSize;
-	initAccels<<<nBlocks, blockSize>>>(d_accels, d_values);
-	EC(cudaDeviceSynchronize());
-
 	dim3 blockSizeGrid(16, 16);
 	dim3 nBlocksGrid(
 		(NUMENTITIES + blockSizeGrid.x - 1) / blockSizeGrid.x,
@@ -83,12 +74,13 @@ extern "C" void compute() {
 	pairwiseAccels<<<nBlocksGrid, blockSizeGrid>>>(d_accels, d_hPos, d_mass);
 	EC(cudaDeviceSynchronize());
 
+	int blockSize = 256;
+	int nBlocks = (NUMENTITIES + blockSize - 1) / blockSize;
+
 	accelSums<<<nBlocks, blockSize>>>(d_accels, d_hPos, d_hVel);
 	EC(cudaDeviceSynchronize());
 
 
-	EC(cudaFree(d_values));
-	EC(cudaFree(d_accels));
 }
 
 extern "C" void initDeviceMemory(int numEntities) {
@@ -99,9 +91,19 @@ extern "C" void initDeviceMemory(int numEntities) {
 	EC(cudaMemcpy(d_hPos, hPos, sizeof(vector3) * NUMENTITIES, cudaMemcpyHostToDevice));
 	EC(cudaMemcpy(d_hVel, hVel, sizeof(vector3) * NUMENTITIES, cudaMemcpyHostToDevice));
 	EC(cudaMemcpy(d_mass, mass, sizeof(double) * NUMENTITIES, cudaMemcpyHostToDevice));
+
+	EC(cudaMalloc(&d_values, sizeof(vector3) * NUMENTITIES * NUMENTITIES));
+	EC(cudaMalloc(&d_accels, sizeof(vector3*) * NUMENTITIES));
+	int blockSize = 256;
+	int nBlocks = (NUMENTITIES + blockSize - 1) / blockSize;
+	initAccels << <nBlocks, blockSize >> > (d_accels, d_values);
+	EC(cudaDeviceSynchronize());
 }
 
 extern "C" void freeDeviceMemory(int numEntities) {
+	EC(cudaFree(d_values));
+	EC(cudaFree(d_accels));
+
 	EC(cudaMemcpy(hPos, d_hPos, sizeof(vector3) * NUMENTITIES, cudaMemcpyDeviceToHost));
 	EC(cudaMemcpy(hVel, d_hVel, sizeof(vector3) * NUMENTITIES, cudaMemcpyDeviceToHost));
 	EC(cudaMemcpy(mass, d_mass, sizeof(double) * NUMENTITIES, cudaMemcpyDeviceToHost));
